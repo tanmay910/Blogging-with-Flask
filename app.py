6048b9bf65a2bcc3caab0c369c026e4729096960
tanmay mahajan <mahajantanmay910@gmail.com>
2023-06-29 11:05:44 +0530

    404 page added

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def index(name):
     return render_template("index.html",username=name)
 
+@app.errorhandler(404)
+def error404(e):
+    return render_template('404.html')
+    
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
